ab119c3bc7f43cf87684e2828c148dcaa4110cc4
jb <angelfire529@yahoo.com>
2018-08-15 21:59:35 -0700

    adding all

--- src/]/models/campaign-creatives.ts
+++
@@ -1,7 +0,0 @@
-import { Campaign } from './campaign.class';
-import { Creative } from './creative';
-
-
-export class CampaignCreatives {
-    constructor(public campaigns: Campaign[], public creatives: Creative[]) {}
-}
